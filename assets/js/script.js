74dd2bb646b5e64d9941f188aef30510242e6e0e
chauhtluu <182552cl@gmail.com>
2025-02-08 04:41:43 -0500

    adjusted web responsiveness

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function showHamburger() {
+    const hamburger = document.querySelector('.hamburger');
+    hamburger.style.display = 'flex'
+}
+
+function hideHamburger() {
+    const hamburger = document.querySelector('.hamburger');
+    hamburger.style.display = 'none'
+}
